f9d69591e986d6e5de60610e07bdb3eba032ac06
Maxime Morlet <MaxiCom.Developpement@outlook.fr>
2023-11-23 23:58:27 +0100

    +ActiveUserCount-ApiResource

--- src/Entity/ActiveUserCount.php
+++ src/Entity/ActiveUserCount.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ActiveUserCountRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ActiveUserCountRepository::class)]
+#[ApiResource]
 class ActiveUserCount
 {
     #[ORM\Id]
